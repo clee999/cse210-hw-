08c126191a83d152106b2c6f0245c492bfc0b8b2
clee999 <64559134+clee999@users.noreply.github.com>
2023-04-18 14:41:16 +0100

    Added comments

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,7 +4,9 @@ class Program
 {
     static void Main(string[] args)
     {
+       //Ouput asking the user for there name 
        Console.Write("What is your first name? ");
+       //input reads and stores value in the string varible
        string fname = Console.ReadLine();
 
        Console.Write("What is your last name? ");
