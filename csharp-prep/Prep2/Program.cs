fcd7b07aaf8432fad151ddffacfb8157910d2dab
clee999 <64559134+clee999@users.noreply.github.com>
2023-04-21 23:40:12 +0100

    Changed code

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+
+          //Ouput asking the user for there name 
+       Console.Write("Enter your grade: ");
+       //input reads and stores value in the string varible
+       string grade = Console.ReadLine();
+       int number = int.Parse(grade);
+
+        if ( number >= 90 ){
+
+            Console.Write("You grade is A ");
+        }
+        else if (number >= 80 ) {
+
+            Console.Write("You grade is B ");
+
+        }
+
+        else {
+
+            Console.Write("You grade is D ");
+
+        }
     }
 }
