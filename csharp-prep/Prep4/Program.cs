d0ad3b5dc880ecc560ff95a89a910945d9c5643b
Carl <carllinfordlee@me.com>
2023-04-26 19:39:45 +0100

    Working progess - added loops

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int>numbers =  new List<int>();
+        int sum;
+        Double average;
+        int largest;
+        Console.WriteLine("Enter a list of numbers, type 0 when finished");
+        int number;
+    do {
+        Console.WriteLine("Enter a number:"); 
+        number = int.Parse(Console.ReadLine());
+        numbers.Add(number);
+        sum = numbers.Sum();
+        average = numbers.Average();
+        largest = numbers.Max();
+
+        } while(number != 0);
+
+        Console.WriteLine($"The sum is {sum}.");
+        Console.WriteLine($"The average is {average}.");
+        Console.WriteLine($"The Largest number is {largest}.");
+
+
     }
+
+
+
 }
