3c5277fb4962f2fa95a8624f95dc48436b85733e
Carl <carllinfordlee@me.com>
2023-04-26 15:34:38 +0100

    adding notes

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,7 +5,7 @@ class Program
     static void Main(string[] args)
     {
         {   
-
+            // need to declare Variables before the while loop 
             int guess_attempted = 0;
             Console.Write("What is the magic number? ");
             int magic_number = int.Parse(Console.ReadLine());
@@ -14,12 +14,12 @@ class Program
             int guess ;
             
             do {
-
+                // this is where the loop starts and the user enter guess
                 Console.Write("What is your guess? ");
                 guess = int.Parse(Console.ReadLine());
                 guess_attempted += 1;
              
-                 
+                 //this if 
                  if (guess == magic_number) {
                      Console.Write($"You've guessed it!, your total amount guesses are {guess_attempted}\n");
                      Console.Write("Would you like to continue? ");
