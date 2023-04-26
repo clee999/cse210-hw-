5ef21312856ba71cf05974489df76c6c3bdec7c3
Carl <carllinfordlee@me.com>
2023-04-26 15:20:45 +0100

    program finished

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        {   
+
+            int guess_attempted = 0;
+            Console.Write("What is the magic number? ");
+            int magic_number = int.Parse(Console.ReadLine());
+
+            string response = "yes";
+            int guess ;
+            
+            do {
+
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+                guess_attempted += 1;
+             
+                 
+                 if (guess == magic_number) {
+                     Console.Write($"You've guessed it!, your total amount guesses are {guess_attempted}\n");
+                     Console.Write("Would you like to continue? ");
+                     response = Console.ReadLine();
+                     Console.Write("What is the magic number? ");
+                     magic_number = int.Parse(Console.ReadLine());
+                     guess_attempted = 0;
+
+                }
+                else if (guess > magic_number) {
+                        Console.Write("Lower\n");
+                }
+                    
+                else if (guess < magic_number) {
+                        Console.Write("Higer\n");
+                }
+
+             } while (guess != magic_number || response == "yes"); 
+        }
+
     }
+        
 }
